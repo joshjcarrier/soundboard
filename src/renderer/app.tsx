7191361abf0be8fa113c4660063faee07eb54b43
Josh Carrier <joshjcarrier@gmail.com>
2019-07-07 16:50:12 -0700

    unfeat(redux): Don't need Redux... for now

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
 import { AppContainer } from 'react-hot-loader';
 
 import Application from './components/Application';
-import store from './store';
 
 require('tachyons/css/tachyons.min.css');
 
@@ -17,9 +15,7 @@ document.body.appendChild(mainElement);
 const render = (Component: () => JSX.Element) => {
     ReactDOM.render(
         <AppContainer>
-            <Provider store={store}>
-                <Component />
-            </Provider>
+              <Component />
         </AppContainer>,
         mainElement
     );
