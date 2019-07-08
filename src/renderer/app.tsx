a045f7a5987bcd67f119d03e662a3f0e958bde75
Josh Carrier <joshjcarrier@gmail.com>
2019-07-07 23:55:42 -0700

    fix(app): Type error

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -8,17 +8,13 @@ require('tachyons/css/tachyons.min.css');
 
 // Create main element
 const mainElement = document.createElement('div');
-mainElement.className = "h-100 overflow-hidden";
+mainElement.className = 'h-100 overflow-hidden';
 document.body.appendChild(mainElement);
 
 // Render components
-const render = (Component: () => JSX.Element) => {
-    ReactDOM.render(
-        <AppContainer>
-              <Component />
-        </AppContainer>,
-        mainElement
-    );
-};
-
-render(Application);
+ReactDOM.render(
+    <AppContainer>
+        <Application />
+    </AppContainer>,
+    mainElement
+);
